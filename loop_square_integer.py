cc0df6036ecbfb3788f005e5953d896cf3b5825c
Raphael Merx <raphael.merx@gmail.com>
2017-01-31 16:33:45 +0900

    Add instructions for homework

---
+++ loop_square_integer.py
@@ -0,0 +1,21 @@
+# Write a function that takes an integer n as argument, and returns the list of the square of all integers i < n.
+
+def f(n):
+    # your code here
+
+# In the end, your function should have the following results:
+
+# In [4]: f(1)
+# Out[4]: [0]
+#
+# In [5]: f(2)
+# Out[5]: [0, 1]
+#
+# In [6]: f(3)
+# Out[6]: [0, 1, 4]
+#
+# In [7]: f(4)
+# Out[7]: [0, 1, 4, 9]
+#
+# In [8]: f(5)
+# Out[8]: [0, 1, 4, 9, 16]
