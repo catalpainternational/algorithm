f8c012a85831ed0334bf081394e5c09cb998ee01
Raphael Merx <raphael.merx@gmail.com>
2017-02-07 10:04:50 +0900

    Add instructions for Feb 29

---
+++ february_29.py
@@ -0,0 +1,9 @@
+# Write a function that takes a year number, and returns the number of days in February.
+
+def f(n):
+    # return True or False
+
+
+f(2017)  # returns 28
+f(2016)  # return 29
+f(2020)  # return 29
