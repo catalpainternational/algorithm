35a89a283091161626a9df5349f3c9afd6321a55
Raphael Merx <raphael.merx@gmail.com>
2017-02-07 12:14:57 +0900

    Add instructions for second highest number

---
+++ second_largest_number.py
@@ -0,0 +1,11 @@
+# Write a function that takes a list of numbers, and returns the second highest number.
+
+def f(lista):
+    # return the second highest number
+    pass
+
+
+f([0, 1, 2])  # returns 1
+f([3, 2, 1])  # returns 2
+f([4, 10, 8, 9])  # returns 9
+f([32, 12, 21, 43, 0])  # returns 32
