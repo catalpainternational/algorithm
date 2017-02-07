32e657c9ce6594ba415d3372e06e2d113f2ee7d4
Raphael Merx <raphael.merx@gmail.com>
2017-02-07 12:04:25 +0900

    Add the solution for february 28 or 29

--- february_29.py
+++ february_29.py
@@ -1,9 +1,26 @@
 # Write a function that takes a year number, and returns the number of days in February.
 
-def f(n):
-    # return True or False
+def ano(n):
+    if n/4.0 == n/4:
+        return 29
+    else:
+        return 28
 
+def ony(n):
+    if n % 4 == 0:
+        return 29
+    else:
+        return 28
 
-f(2017)  # returns 28
-f(2016)  # return 29
-f(2020)  # return 29
+def niko_and_mario(n):
+    if n in range(0, n+1, 4):
+        return 29
+    else:
+        return 28
+
+# f(2017) returns 28
+# f(2016) return 29
+# f(2020) return 29
+
+# To time the function in ipython:
+# %timeit for x in range(1900, 2100): niko_and_mario(x)
