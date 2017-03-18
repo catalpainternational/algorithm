664dc852d1b41699058c280bf2bde303e40ba614
Raphael Merx <raphael.merx@gmail.com>
2017-03-18 12:19:35 +0900

    Add solution for second_largest_number

--- second_largest_number.py
+++ second_largest_number.py
@@ -1,11 +1,34 @@
 # Write a function that takes a list of numbers, and returns the second highest number.
 
-def f(lista):
+def second_largest_number(lista):
     # return the second highest number
     pass
 
 
-f([0, 1, 2])  # returns 1
-f([3, 2, 1])  # returns 2
-f([4, 10, 8, 9])  # returns 9
-f([32, 12, 21, 43, 0])  # returns 32
+second_largest_number([0, 1, 2])  # returns 1
+second_largest_number([3, 2, 1])  # returns 2
+second_largest_number([4, 10, 8, 9])  # returns 9
+second_largest_number([32, 12, 21, 43, 0])  # returns 32
+
+def niko(lista):
+    first, second = None, None
+    for n in lista:
+        if n > first:
+            first, second = n, first
+        elif first > n > second:
+            second = n
+    return second
+
+def ano(lista):
+    listas = []
+    max_lista = max(lista)
+    for item in lista:
+        if item < max_lista:
+            listas.append(item)
+    return max(listas)
+
+def rapha(lista):
+    max_lista = max(lista)
+    max_index = lista.index(max_lista)
+    lista.pop(max_index)
+    return max(lista)
