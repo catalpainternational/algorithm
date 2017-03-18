2afaf8f936cb2456f909fda9c62487a112649f82
Raphael Merx <raphael.merx@gmail.com>
2017-03-18 12:30:06 +0900

    Add hw for string replacement

---
+++ string_insert.py
@@ -0,0 +1,12 @@
+def replace_char(string, index, char):
+    # returns modified string with index replaced with char
+    pass
+
+
+replace_char('foo', 0, 'b')  # returns 'boo'
+replace_char('foo', 1, 'b')  # returns 'fbo'
+replace_char('foo', 2, 'b')  # returns 'fob'
+replace_char('mario', 2, 't')  # returns 'matio'
+
+replace_char('ta', 0, 'h')  # returns 'ha'
+replace_char('ha', 1, 'o')  # returns 'ho'
